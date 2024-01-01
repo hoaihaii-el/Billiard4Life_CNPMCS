f049ab9c71e66b1fe7f264a19b0d1cf50538dc08
Phu Le <21522466@gm.uit.edu.vn>
2024-01-01 23:11:13 +0700

    [TinhTrangBanViewModel] fix bug tinhtrangban

--- Billiard4Life/ViewModel/TinhTrangBanViewModel.cs
+++ Billiard4Life/ViewModel/TinhTrangBanViewModel.cs
@@ -1,4 +1,36 @@
-﻿namespace Billiard4Life.ViewModel
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using Billiard4Life.Models;
+using Billiard4Life.View;
+using RestaurantManagement.Models;
+using TinhTrangBan.Models;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Windows.Forms;
+using OfficeOpenXml.ConditionalFormatting;
+using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+using Billiard4Life.DataProvider;
+using Org.BouncyCastle.Math;
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+using Documment = iTextSharp.text.Document;
+using System.IO;
+using System.Windows.Data;
+using System.ComponentModel;
+using KhuyenMai = Billiard4Life.Models.KhuyenMai;
+using OfficeOpenXml.FormulaParsing.ExpressionGraph.FunctionCompilers;
+
+namespace Billiard4Life.ViewModel
 {
     public class TinhTrangBanViewModel : BaseViewModel
     {
